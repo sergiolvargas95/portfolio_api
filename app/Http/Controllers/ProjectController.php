01dbeb9d01408488611a67cd6b2d147c2d14564f
sergiolvargas95 <sergiolvargas95@gmail.com>
2025-03-27 18:35:14 -0500

    add: request to manage validation in projects store method

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProjectRequest;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
@@ -18,27 +19,8 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request)
     {
-        $request->validate([
-            'title' => 'required|string|max:200',
-            'description' => 'required|string|max:255',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,webp|max:2048',
-            'repository_url' => 'required|url|max:255',
-            'demo_url' => 'nullable|url|max:255',
-            'user_id' => 'required|exists:users,id'
-        ], [
-            'title.required' => 'El título es obligatorio.',
-            'title.max' => 'El título no debe superar los 200 caracteres.',
-            'description.required' => 'La descripción es obligatoria.',
-            'image.image' => 'El archivo debe ser una imagen.',
-            'image.mimes' => 'Las imágenes permitidas son JPEG, PNG, JPG y WEBP.',
-            'repository_url.required' => 'El enlace del repositorio es obligatorio.',
-            'repository_url.url' => 'El enlace del repositorio debe ser una URL válida.',
-            'user_id.required' => 'El usuario es obligatorio.',
-            'user_id.exists' => 'El usuario seleccionado no es válido.'
-        ]);
-
         if ($request->hasFile('image')) {
             $imagePath = $request->file('image')->store('projects', 'public');
         } else {
