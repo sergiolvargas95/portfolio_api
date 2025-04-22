acc4239825590224fe5263712bc51f76359f90d7
sergiolvargas95 <sergiolvargas95@gmail.com>
2025-04-21 22:19:44 -0500

    fix: origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['https://sergiolvargas95.github.io/portfolio_admin_frontend'],
 
     'allowed_origins_patterns' => [],
 
