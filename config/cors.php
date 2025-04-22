945e8c51a47e4991e50c44a5f47cea341f7d7ce4
Sergio Vargas <65960993+sergiolvargas95@users.noreply.github.com>
2025-04-22 08:45:24 -0500

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sergiolvargas95.github.io/portfolio_admin_frontend'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
